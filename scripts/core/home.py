7782c13b1802cd87c2d09b0e2143db6d0a5e19cc
Ivan Masci <ivan.masci@outlook.com>
2026-07-26 10:11:50 +0200

    Homepage sections

--- scripts/core/home.py
+++ scripts/core/home.py
@@ -108,11 +108,11 @@ hide:
 
 {banner_html}
 
-<div class="home-columns" markdown="1">
+<div class="home-columns">
 
-<div class="home-column" markdown="1">
+<div class="home-column">
 
-## 📥 Aggiunti di recente
+<h2>📥 Aggiunti di recente</h2>
 
 <div class="recent-container">
 
@@ -142,9 +142,9 @@ hide:
 
 </div>
 
-<div class="home-column" markdown="1">
+<div class="home-column">
 
-## 👤 Persone più menzionate
+<h2>👤 Persone più menzionate</h2>
 
 <div class="recent-container">
 
